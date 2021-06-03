82352e7f76106a60314e2957046aee3f15cf21ae
Polygon <79771454+PoIygon@users.noreply.github.com>
2021-06-03 21:14:26 +1000

    Create header.js

---
+++ resources/js/header.js
@@ -0,0 +1,5 @@
+document.getElementById("header").appendChild(<a href="/pages/home">Home</a>
+        <a href="/pages/blog">Blog</a>
+        <a href="/pages/contact">Contact</a>
+        <a href="https://scratch.mit.edu/users/PoIygon">Scratch</a>
+        <a href="https://postpercent.rirurin.com/users/PoIygon">PostPercent</a>)
